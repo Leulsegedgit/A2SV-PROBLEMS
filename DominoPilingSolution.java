ec4b4ab7c786e0e88ff93b808054724ac2e8dca9
Leulsegedgit <64631411+Leulsegedgit@users.noreply.github.com>
2021-12-09 19:31:15 +0300

    Create DominoPilingSolution.java

---
+++ DominoPilingSolution.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+     
+public class DominoPilingSolution {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        int m , n;
+        m = input.nextInt();
+        n = input.nextInt();
+        System.out.println(m*n/2);
+        
+    }
+    
+}
