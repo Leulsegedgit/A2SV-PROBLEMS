8d19677aed3a4c4e3f12b8ce76ad7aafd327b5c2
Leulsegedgit <64631411+Leulsegedgit@users.noreply.github.com>
2022-01-20 19:50:05 +0300

    Create isValidParenthesis.java

---
+++ isValidParenthesis.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for(int i=0; i<s.length(); i++){
+            if((!stack.isEmpty())&& (s.charAt(i) == ')' && stack.peek() == '(' || s.charAt(i) == ']' && stack.peek() == '[' || s.charAt(i) == '}' && stack.peek() == '{' ))
+                stack.pop();
+            else
+                stack.push(s.charAt(i));
+                
+                    }
+        if(stack.isEmpty())
+            return true;
+        else return false;
+    }
+}
